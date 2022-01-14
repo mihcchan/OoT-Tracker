abb9b4db2aae537472ef64b318b1dc659e679de5
Mihcchan <michelle.gmurczyk@usp.br>
2022-01-14 18:54:06 -0300

    added working chronometer

--- app/src/main/java/com/example/oot_tracker_2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/oot_tracker_2/ui/home/HomeFragment.kt
@@ -12,7 +12,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.oot_tracker_2.R
 import com.example.oot_tracker_2.databinding.FragmentHomeBinding
-import hallianinc.opensource.timecounter.StopWatch
+import android.widget.Chronometer
+
+import android.widget.Chronometer.OnChronometerTickListener
+import com.example.oot_tracker_2.ChronometerWithMS
+
 
 class HomeFragment : Fragment() {
 
@@ -24,6 +28,8 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private var currentStrength: Int = 0
+
+    private var pauseOffSet = 0L
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,23 +47,38 @@ class HomeFragment : Fragment() {
             strengthChange(strengthButton)
         }
 
-        val stopwatch = StopWatch(binding.chronometer)
-        //var time = stopwatch.time
+        val chronometer: ChronometerWithMS = binding.chronometer2
         val startStopButton: ToggleButton = binding.playPauseButton
         val stopButton: ImageButton = binding.stopButton
+
         startStopButton.setOnClickListener {
             if (startStopButton.isChecked) {
-                stopwatch.resume()
+                chronometer.restart()
             } else {
-                stopwatch.pause()
+                chronometer.stop()
             }
         }
 
         stopButton.setOnClickListener {
-            stopwatch.stop()
+            if (startStopButton.isChecked) {
+                startStopButton.toggle()
+            }
+            chronometer.stop()
+            chronometer.resetTime()
         }
 
 //        val chronometer : Chronometer = binding.chronometer
+//
+//        chronometer.onChronometerTickListener = OnChronometerTickListener { cArg ->
+//            val time = SystemClock.elapsedRealtime() - cArg.base
+//            val h = (time / 3600000).toInt()
+//            val m = (time - h * 3600000).toInt() / 60000
+//            val s = (time - h * 3600000 - m * 60000).toInt() / 1000
+//            val hh = if (h < 10) "0$h" else h.toString() + ""
+//            val mm = if (m < 10) "0$m" else m.toString() + ""
+//            val ss = if (s < 10) "0$s" else s.toString() + ""
+//            cArg.text = "$hh:$mm:$ss"
+//        }
 //
 //        val startStopButton : ToggleButton = binding.playPauseButton
 //        val stopButton : ImageButton = binding.stopButton
@@ -72,6 +93,10 @@ class HomeFragment : Fragment() {
 //        }
 //
 //        stopButton.setOnClickListener{
+//            if (startStopButton.isChecked){
+//                startStopButton.toggle()
+//            }
+//            chronometer.stop()
 //            pauseOffSet = 0L
 //            chronometer.base = SystemClock.elapsedRealtime()
 //        }
@@ -81,15 +106,20 @@ class HomeFragment : Fragment() {
 
     private fun strengthChange(strengthButton: ImageButton) {
         currentStrength++
-        if (currentStrength == 1) {
-            strengthButton.setImageResource(R.drawable.oot3d_goron_bracelet_icon)
-        } else if (currentStrength == 2) {
-            strengthButton.setImageResource(R.drawable.oot3d_silver_gauntlets_icon)
-        } else if (currentStrength == 3) {
-            strengthButton.setImageResource(R.drawable.oot3d_golden_gauntlets_icon)
-        } else {
-            strengthButton.setImageResource(R.drawable.oot3d_goron_bracelet_icon_bw)
-            currentStrength = 0
+        when (currentStrength) {
+            1 -> {
+                strengthButton.setImageResource(R.drawable.oot3d_goron_bracelet_icon)
+            }
+            2 -> {
+                strengthButton.setImageResource(R.drawable.oot3d_silver_gauntlets_icon)
+            }
+            3 -> {
+                strengthButton.setImageResource(R.drawable.oot3d_golden_gauntlets_icon)
+            }
+            else -> {
+                strengthButton.setImageResource(R.drawable.oot3d_goron_bracelet_icon_bw)
+                currentStrength = 0
+            }
         }
     }
 
