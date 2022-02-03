86f66c806e005c881f98083694ae75401f7bf4dd
Mihcchan <michelle.gmurczyk@usp.br>
2022-02-03 19:54:47 -0300

    changed to tgl to imagebutton and sizes matches constraints now

--- app/src/main/java/com/example/oot_tracker_2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/oot_tracker_2/ui/home/HomeFragment.kt
@@ -27,8 +27,15 @@ class HomeFragment : Fragment() {
 
     private var currentStrength: Int = 0
     private var currentHookshot: Int = 0
+    private var currentMagic: Int = 0
+    private var slingShotClicked: Boolean = false
     private var fireArrowClicked: Boolean = false
     private var lightArrowClicked: Boolean = false
+    private var faroreClicked: Boolean = false
+    private var dinClicked: Boolean = false
+    private var ironClicked: Boolean = false
+    private var hoverClicked: Boolean = false
+
     //private var pauseOffSet = 0L
 
     override fun onCreateView(
@@ -43,9 +50,14 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
 
-        val strengthButton: ImageButton = binding.strengthButton
-        strengthButton.setOnClickListener {
-            strengthChange(strengthButton)
+//        val strengthButton: ImageButton = binding.strengthButton
+//        strengthButton.setOnClickListener {
+//            strengthChange(strengthButton)
+//        }
+
+        val slingShotButton: ImageButton = binding.slingshotButton
+        slingShotButton.setOnClickListener {
+            slingShotClicked(slingShotButton)
         }
 
         val hookshotButton: ImageButton = binding.hookshotButton
@@ -61,18 +73,48 @@ class HomeFragment : Fragment() {
             lightArrowClick(fireLightArrowButton)
         }
 
+        val faroreDinButton: ImageButton = binding.faroreDinButton
+        faroreDinButton.setOnLongClickListener(OnLongClickListener {
+            dinClick(faroreDinButton)
+        })
+        faroreDinButton.setOnClickListener {
+            faroreClick(faroreDinButton)
+        }
+
+        val magicButton: ImageButton = binding.magicButton
+        magicButton.setOnClickListener {
+            magicChange(magicButton)
+        }
+
+        val hoverIronBootsButton: ImageButton = binding.hoverIronBootsButton
+        hoverIronBootsButton.setOnLongClickListener(OnLongClickListener {
+            hoverBootsClick(hoverIronBootsButton)
+        })
+        hoverIronBootsButton.setOnClickListener {
+            ironBootsClick(hoverIronBootsButton)
+        }
         return root
     }
 
+    private fun slingShotClicked(slingShotButton: ImageButton) {
+        if (!slingShotClicked) {
+            slingShotButton.setImageResource(R.drawable.oot3d_fairy_slingshot_icon)
+            slingShotClicked = true
+        } else {
+            slingShotButton.setImageResource(R.drawable.oot3d_fairy_slingshot_icon_bw)
+            slingShotClicked = false
+        }
+    }
+
     private fun lightArrowClick(fireLightArrowButton: ImageButton) {
-        if (!lightArrowClicked && !fireArrowClicked) {
+        if (!fireArrowClicked && !lightArrowClicked) {
             fireLightArrowButton.setImageResource(R.drawable.oot3d_half_light_arrow)
             lightArrowClicked = true
         } else if (fireArrowClicked && !lightArrowClicked) {
             fireLightArrowButton.setImageResource(R.drawable.oot3d_fire_light_arrow)
             lightArrowClicked = true
         } else if (!fireArrowClicked && lightArrowClicked) {
-            fireLightArrowButton.setImageResource(R.drawable.oot3d_fire_ice_arrow_bw)
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_fire_light_arrow_bw)
             lightArrowClicked = false
         } else if (fireArrowClicked && lightArrowClicked) {
             fireLightArrowButton.setImageResource(R.drawable.oot3d_half_fire_arrow)
@@ -88,11 +130,77 @@ class HomeFragment : Fragment() {
             fireLightArrowButton.setImageResource(R.drawable.oot3d_fire_light_arrow)
             fireArrowClicked = true
         } else if (fireArrowClicked && !lightArrowClicked) {
-            fireLightArrowButton.setImageResource(R.drawable.oot3d_fire_ice_arrow_bw)
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_fire_light_arrow_bw)
             fireArrowClicked = false
         } else if (fireArrowClicked && lightArrowClicked) {
             fireLightArrowButton.setImageResource(R.drawable.oot3d_half_light_arrow)
             fireArrowClicked = false
+        }
+        return true
+    }
+
+    private fun faroreClick(faroreDinButton: ImageButton) {
+        if (!dinClicked && !faroreClicked) {
+            faroreDinButton.setImageResource(R.drawable.oot3d_half_farore_s_wind_icon)
+            faroreClicked = true
+        } else if (dinClicked && !faroreClicked) {
+            faroreDinButton.setImageResource(R.drawable.oot3d_farore_s_din_s_fire_icon)
+            faroreClicked = true
+        } else if (!dinClicked && faroreClicked) {
+            faroreDinButton.setImageResource(R.drawable.oot3d_farore_s_wind_icon_bw)
+            faroreClicked = false
+        } else if (dinClicked && faroreClicked) {
+            faroreDinButton.setImageResource(R.drawable.oot3d_half_din_s_fire_icon)
+            faroreClicked = false
+        }
+    }
+
+    private fun dinClick(faroreDinButton: ImageButton): Boolean {
+        if (!dinClicked && !faroreClicked) {
+            faroreDinButton.setImageResource(R.drawable.oot3d_half_din_s_fire_icon)
+            dinClicked = true
+        } else if (!dinClicked && faroreClicked) {
+            faroreDinButton.setImageResource(R.drawable.oot3d_farore_s_din_s_fire_icon)
+            dinClicked = true
+        } else if (dinClicked && !faroreClicked) {
+            faroreDinButton.setImageResource(R.drawable.oot3d_din_s_fire_icon_bw)
+            dinClicked = false
+        } else if (dinClicked && faroreClicked) {
+            faroreDinButton.setImageResource(R.drawable.oot3d_half_farore_s_wind_icon)
+            dinClicked = false
+        }
+        return true
+    }
+
+    private fun ironBootsClick(fireLightArrowButton: ImageButton) {
+        if (!hoverClicked && !ironClicked) {
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_half_iron_boots_icon)
+            ironClicked = true
+        } else if (hoverClicked && !ironClicked) {
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_hover_iron_boots_icon)
+            ironClicked = true
+        } else if (!hoverClicked && ironClicked) {
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_hover_iron_boots_icon_bw)
+            ironClicked = false
+        } else if (hoverClicked && ironClicked) {
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_half_hover_boots_icon)
+            ironClicked = false
+        }
+    }
+
+    private fun hoverBootsClick(fireLightArrowButton: ImageButton): Boolean {
+        if (!hoverClicked && !ironClicked) {
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_half_hover_boots_icon)
+            hoverClicked = true
+        } else if (!hoverClicked && ironClicked) {
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_hover_iron_boots_icon)
+            hoverClicked = true
+        } else if (hoverClicked && !ironClicked) {
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_hover_iron_boots_icon_bw)
+            hoverClicked = false
+        } else if (hoverClicked && ironClicked) {
+            fireLightArrowButton.setImageResource(R.drawable.oot3d_half_iron_boots_icon)
+            hoverClicked = false
         }
         return true
     }
@@ -116,8 +224,23 @@ class HomeFragment : Fragment() {
         }
     }
 
+    private fun magicChange(magicButton: ImageButton) {
+        currentMagic++
+        when (currentMagic) {
+            1 -> {
+                magicButton.setImageResource(R.drawable.oot3d_magic_jar)
+            }
+            2 -> {
+                magicButton.setImageResource(R.drawable.oot3d_magic_jar_big)
+            }
+            else -> {
+                magicButton.setImageResource(R.drawable.oot3d_magic_jar_bw)
+                currentMagic = 0
+            }
+        }
+    }
+
     private fun hookshotChange(hookshotButton: ImageButton) {
-        val hookshotImage: ImageView = binding.hookshotImage
         currentHookshot++
         when (currentHookshot) {
             1 -> {
@@ -125,11 +248,9 @@ class HomeFragment : Fragment() {
             }
             2 -> {
                 hookshotButton.setImageResource(R.drawable.oot3d_longshot_icon)
-                hookshotImage.setImageResource(R.drawable.oot3d_longshot_icon)
             }
             else -> {
                 hookshotButton.setImageResource(R.drawable.oot3d_hookshot_icon_bw)
-                hookshotImage.setImageResource(R.drawable.oot3d_hookshot_icon_bw)
                 currentHookshot = 0
             }
         }
